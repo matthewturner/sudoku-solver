fa2892e09bccba6098493d8555a0e3df93a54b19
Matt Turner <matt@fiverings.uk>
2022-06-21 08:02:55 +0100

    Add column

---
+++ src/column.py
@@ -0,0 +1,10 @@
+from array import array
+
+
+class Column:
+    def __init__(self, index: int, values: array):
+        self.index = index
+        self.values = values
+
+    def is_valid(self, value: int):
+        return not value in self.values[self.index]
---
+++ src/test_column.py
@@ -0,0 +1,18 @@
+from column import Column
+import pytest
+
+
+def test_is_valid():
+    values = [[1, 2, None, 4],
+              [None, 1, 2, 4]]
+    target = Column(0, values)
+    actual = target.is_valid(2)
+    assert actual
+
+
+def test_is_valid():
+    values = [[1, 2, None, 4],
+              [2, 1, 3, 4]]
+    target = Column(0, values)
+    actual = not target.is_valid(2)
+    assert actual
