4ac75538d7b03abb4f0627a75a711b273ca76514
Matt Turner <matt@fiverings.uk>
2022-06-24 07:41:40 +0100

    Add note taking

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -6,9 +6,11 @@ class Solver:
         self.row_change_listener = None
 
     def solve(self, puzzle: Puzzle):
-        return self.__solve(puzzle, 0, 0)
+        notes = self.__create_notes(puzzle)
+        self.__apply_single_candidates(puzzle, notes)
+        return self.__solve(puzzle, notes, 0, 0)
 
-    def __solve(self, puzzle: Puzzle, column: int, row: int):
+    def __solve(self, puzzle: Puzzle, notes: dict, column: int, row: int):
         if row == puzzle.size:
             if not self.row_change_listener is None:
                 self.row_change_listener(puzzle)
@@ -19,13 +21,32 @@ class Solver:
             row = 0
 
         if puzzle.has_value(column, row):
-            return self.__solve(puzzle, column, row + 1)
+            return self.__solve(puzzle, notes, column, row + 1)
 
-        for candidate in puzzle.candidates:
+        for candidate in notes[(column, row)]:
             if puzzle.try_set(column, row, candidate):
-                if self.__solve(puzzle, column, row + 1):
+                if self.__solve(puzzle, notes, column, row + 1):
                     return True
                 else:
                     puzzle.clear(column, row)
 
         return False
+
+    def __apply_single_candidates(self, puzzle: Puzzle, notes: dict):
+        for location in notes:
+            note = notes[location]
+            if len(note) == 1:
+                column, row = location
+                puzzle.set(column, row, note[0])
+                note.clear()
+
+    def __create_notes(self, puzzle: Puzzle):
+        notes = {}
+        for row in range(0, puzzle.size):
+            for column in range(0, puzzle.size):
+                if not puzzle.has_value(column, row):
+                    candidates = notes[(column, row)] = []
+                    for candidate in puzzle.candidates:
+                        if puzzle.is_valid(column, row, candidate):
+                            candidates.append(candidate)
+        return notes
