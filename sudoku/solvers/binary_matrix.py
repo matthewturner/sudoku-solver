0c7ced4065abb1919c4c482d9ec48058d97a11d2
Matt Turner <matt@fiverings.uk>
2022-06-28 08:02:02 +0100

    Fix row constraint and add column constraint

--- sudoku/solvers/binary_matrix.py
+++ sudoku/solvers/binary_matrix.py
@@ -18,6 +18,7 @@ class BinaryMatrix:
         bm.__apply_cell_constraint(puzzle)
         bm.__apply_row_constraint(puzzle)
         bm.__apply_column_constraint(puzzle)
+        bm.__apply_box_constraint(puzzle)
 
         return bm
 
@@ -31,10 +32,20 @@ class BinaryMatrix:
     def __apply_row_constraint(self, puzzle: Puzzle):
         padding = -puzzle.size
         for row in range(0, len(self.matrix)):
-            column = row % puzzle.size + padding
-            self.matrix[row, column] = True
             if row % puzzle.size ** 2 == 0:
                 padding += puzzle.size
+            column = row % puzzle.size + padding
+            self.matrix[row, column] = True
 
     def __apply_column_constraint(self, puzzle: Puzzle):
+        padding = -puzzle.size
+        for row in range(0, len(self.matrix)):
+            if row % puzzle.size == 0:
+                padding += puzzle.size
+            if row % puzzle.size ** 2 == 0:
+                padding = 0
+            column = row % puzzle.size + padding
+            self.matrix[row, column] = True
+
+    def __apply_box_constraint(self, puzzle: Puzzle):
         pass
